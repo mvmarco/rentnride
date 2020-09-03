aebca4178a71f8a47444a6c94d917c4efc813d11
uros simcic <inbox.si@gmail.com>
2020-09-03 12:57:45 -0300

    car add, delete, edit

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,19 +1,35 @@
 class CarsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+  def index
+    @cars = Car.all
+  end
 
   def new
     @car = Car.new # car form instantiation
   end
-  
+
   def show
     @car = Car.find(params[:id])
   end
-  
+
   def create
     @car = Car.new(car_params)
+    #@car = Car.new(params[:car])
     @car.user = current_user
-    @car.save
+    @car.save!
 
     redirect_to car_path(@car)
+  end
+
+  def edit
+    @car = Car.find(params[:id])
+  end
+
+   def destroy
+    @car = Car.find(params[:id])
+    @car.destroy
+
+    redirect_to cars_paths
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,22 @@
 Rails.application.routes.draw do
+
   devise_for :users
   resources :cars
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #to be checked
+
+  #get "cars", to: "cars#index"
+
+  #get "cars/new", to: "cars#new"
+
+  #post "cars", to: "cars#create"
+
+  #get "cars/:id/edit", to: "cars#edit"
+
+  #no patch
+
+  #delete "cars/:id", to: "cars#destroy"
+
 end
