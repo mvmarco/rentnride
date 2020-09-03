5b322f64244b9179209a64d2af5685b1fede90d9
uros simcic <inbox.si@gmail.com>
2020-09-03 16:12:15 -0300

    crud finished

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -29,11 +29,18 @@ class CarsController < ApplicationController
     @car = Car.find(params[:id])
   end
 
+  def update
+    @car = Car.find(params[:id])
+    @car.update(car_params)
+    @car.save
+    redirect_to car_path(@car)
+  end
+
    def destroy
     @car = Car.find(params[:id])
     @car.destroy
 
-    redirect_to cars_paths
+    redirect_to root_path
   end
 
   private
